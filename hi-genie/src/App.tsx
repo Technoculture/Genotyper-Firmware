59856f0146662edec041ea0d0f4340c2db4ad515
Satyam Tiwary <satyam@technoculture.io>
2023-02-22 17:18:51 +0530

    chore: simplify tauri setup

--- hi-genie/src/App.tsx
+++ hi-genie/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+import * as Popover from '@radix-ui/react-popover';
+// import { motion, AnimatePresence } from "framer-motion";
 
 function App () {
   const [greetMsg, setGreetMsg] = useState("");
@@ -12,20 +13,33 @@ function App () {
   }
 
   return (
-    <div className="container">
-      <div className="text-3xl font-bold underline">Welcome to Tauri!</div>
+    <div className="p-2">
+      <div className="text-3xl font-bold">Hi Genie!</div>
 
-      <div className="row">
+      <div>
         <form
           onSubmit={ (e) => {
             e.preventDefault();
             greet();
           } }
+          className="flex gap-1"
         >
+
+          <Popover.Root>
+            <Popover.Trigger className="bg-green-200 p-2 rounded-md">More info</Popover.Trigger>
+            <Popover.Portal>
+              <Popover.Content className="bg-green-50 p-2 rounded-md">
+                Some more info…
+                <Popover.Arrow />
+              </Popover.Content>
+            </Popover.Portal>
+          </Popover.Root>
+
           <input
             id="greet-input"
             onChange={ (e) => setName(e.currentTarget.value) }
             placeholder="Enter a name..."
+            className="bg-red-100 rounded-md p-2"
           />
           <button type="submit">Greet</button>
         </form>
