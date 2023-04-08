5f215c7fea6b42574a581fff24a4a8d840fdba5e
Satyam Tiwary <satyam@technoculture.io>
2023-04-08 18:10:30 +0530

    chore: seperate out the config files

---
+++ scripts/config/README.md
@@ -0,0 +1,3 @@
+
+Currently the genotyper config directory resides here, might be necessary to shift it to a file storage
+	
--- workflow/bin/nodes.rs
+++ workflow/bin/nodes.rs
@@ -1,14 +1,14 @@
 use serde::{Deserialize, Serialize};
 use serde_yaml;
+use std::collections::HashMap;
 use std::fs::File;
-use std::io::Write;
-//use std::collections::HashMap;
+use std::path::PathBuf;
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Library {
-    modules: Vec<Module>,
-    tools: Vec<Tool>,
-    nodes: Vec<KnownNode>,
+    modules: HashMap<String, Module>,
+    tools: HashMap<String, Tool>,
+    nodes: HashMap<String, KnownNode>,
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
@@ -17,7 +17,6 @@ struct Module {
     #[serde(rename = "type")]
     module_type: String,
     address: String,
-    abbr: String,
     description: String,
 }
 
@@ -27,7 +26,6 @@ struct Tool {
     description: String,
     //#[serde(default)]
     pick_up: Option<String>,
-    abbr: String,
     //#[serde(default)]
     variants: Option<Vec<Variant>>,
 }
@@ -36,39 +34,32 @@ struct Tool {
 struct Variant {
     name: String,
     description: Option<String>,
-    abbr: String,
     preffered_when: String,
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
 enum NodeType {
-    #[serde(rename = "condition")]
     Condition,
-    #[serde(rename = "action")]
     Action,
-    #[serde(rename = "sequence")]
     Sequence,
-    #[serde(rename = "error")]
     Error,
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct KnownNode {
-    name: String,
     #[serde(rename = "type")]
     node_type: NodeType,
-    //#[serde(default)]
+    #[serde(default)]
     zenoh: Option<Zenoh>,
     description: String,
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
 enum ReplyMode {
-    #[serde(rename = "from_any")]
     FromAny,
-    #[serde(rename = "from_all")]
     FromAll,
-    #[serde(rename = "from_one")]
     FromOne,
 }
 
@@ -83,141 +74,205 @@ struct Node {
     name: String,
     error: Option<String>,
     children: Option<Vec<Node>>,
+    fallback: Option<Vec<Node>>,
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct BehaviorTreeFile {
-    name: String,
-    task_name: String,
+    title: String,
     version: String,
     description: String,
     #[serde(rename = "participant_modules")]
     participants: Vec<String>,
-    tree: Vec<Node>
+    tree: Node,
 }
 
-fn serialize_behaviour_tree(tree: &BehaviorTreeFile, file_name: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let yaml_string = serde_yaml::to_string(&tree).expect("Failed to serialize tree");
-    let mut file = File::create(file_name).expect("Failed to create file");
-    file.write_all(yaml_string.as_bytes()).expect("Failed to write to file");
-    Ok(())
-}
+//fn serialize_behaviour_tree(tree: &BehaviorTreeFile, file_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+//    let yaml_string = serde_yaml::to_string(&tree).expect("Failed to serialize tree");
+//    let mut file = File::create(file_name).expect("Failed to create file");
+//    file.write_all(yaml_string.as_bytes()).expect("Failed to write to file");
+//    Ok(())
+//}
 
-fn deserialize_behaviour_tree(file_name: &str) -> Result<BehaviorTreeFile, Box<dyn std::error::Error>> {
+fn load_a_behaviour_tree(file_name: &str) -> Result<BehaviorTreeFile, Box<dyn std::error::Error>> {
     let file = File::open(file_name).expect("Failed to open file");
     let tree: BehaviorTreeFile = serde_yaml::from_reader(file).expect("Failed to deserialize tree");
+    println!("{:#?}", tree);
     Ok(tree)
 }
 
-fn extract_names(node: &Node, names: &mut Vec<String>) {
-    names.push(node.name.clone());
-    if let Some(children) = &node.children {
-        for child in children {
-            extract_names(child, names);
-        }
-    }
-}
+//fn extract_names(node: &Node, names: &mut Vec<String>) {
+//    names.push(node.name.clone());
+//    if let Some(children) = &node.children {
+//        for child in children {
+//            extract_names(child, names);
+//        }
+//    }
+//}
 
-fn load_file_modules(path: String) -> Result<Vec<Module>, Box<dyn std::error::Error>> {
+fn load_file_modules(path: &str) -> Result<HashMap<String, Module>, Box<dyn std::error::Error>> {
     // Load the modules
     let modules_file =
-        File::open(path).expect("Unable to open file");
-    let modules: Vec<Module> =
+        File::open(&path)
+            .expect(format!("Unable to open file {}", &path).as_str());
+    let modules: HashMap<String, Module> =
         serde_yaml::from_reader(modules_file).expect("Unable to parse modules");
     println!("Modules file is valid and parsed correctly");
-
     Ok(modules)
 }
 
-fn load_file_tools(path: String) -> Result<Vec<Tool>, Box<dyn std::error::Error>> {
+fn load_file_tools(path: &str) -> Result<HashMap<String, Tool>, Box<dyn std::error::Error>> {
     // Load the tools
-    let tools_file =
-        File::open(path).expect("Unable to open file");
-    let tools: Vec<Tool> =
+    let tools_file = File::open(path).expect("Unable to open file");
+    let tools: HashMap<String, Tool> =
         serde_yaml::from_reader(tools_file).expect("Unable to parse tools");
     println!("Tools file is valid and parsed correctly");
-
     Ok(tools)
 }
 
-fn load_file_nodes(path: String) -> Result<Vec<KnownNode>, Box<dyn std::error::Error>> {
+fn load_file_nodes(
+    path: &str,
+) -> Result<HashMap<String, KnownNode>, Box<dyn std::error::Error>> {
     // Load the nodes
-    let nodes_file =
-        File::open(path).expect("Unable to open file");
-    let nodes: Vec<KnownNode> =
+    let nodes_file = File::open(path).expect("Unable to open file");
+    let nodes: HashMap<String, KnownNode> =
         serde_yaml::from_reader(nodes_file).expect("Unable to parse nodes");
     println!("Nodes file is valid and parsed correctly");
-
     Ok(nodes)
 }
 
-
-fn dependencies_abbr(modules: &Vec<Module>, tools: &Vec<Tool>) -> Vec<String> {
+fn dependencies_abbr(
+    modules: &HashMap<String, Module>,
+    tools: &HashMap<String, Tool>,
+) -> Vec<String> {
     // modules and tools are the same, so we need to collect both their abbr in a single vector
-    let modules_abbrs: Vec<String> = modules
-        .iter()
-        .map(|item| item.abbr.clone())
-        .collect();
-    let tool_abbrs: Vec<String> = tools
-        .iter()
-        .map(|item| item.abbr.clone())
-        .collect();
-    let known_dependencies = [&modules_abbrs[..], &tool_abbrs[..]].concat();
-
-    println!("Modules and Tools in Library: {:?}", &known_dependencies);
-
-    known_dependencies
+    let mut abbrs = Vec::new();
+    for (name, _) in modules {
+        abbrs.push(name.clone());
+    }
+    for (name, _) in tools {
+        abbrs.push(name.clone());
+    }
+    println!("Modules and Tools in Library: {:?}", &abbrs);
+    abbrs
 }
 
-fn main() -> Result<(), serde_yaml::Error> {
-
-    // Read all YAML files
-    // ----
-    //
-    let library_path = "library/";
-    let nodes_file_name = "nodes.yaml";
-    let modules_file_name = "modules.yaml";
-    let tools_file_name = "tools.yaml";
-
-    let modules = load_file_modules(library_path.to_owned() + modules_file_name).expect("Failed to load modules");
-    let tools = load_file_tools(library_path.to_owned() + tools_file_name).expect("Failed to load tools");
-
-    let known_dependencies = dependencies_abbr(&modules, &tools);
-    let nodes = load_file_nodes(library_path.to_owned() + nodes_file_name).expect("Failed to load nodes");
-
+fn validate_nodes_library(
+    nodes: &HashMap<String, KnownNode>,
+    known_dependencies: &Vec<String>,
+) -> Result<(), Box<dyn std::error::Error>> {
     // Throw an error if a node is using a module that is not known
-    nodes.iter().for_each(|n| {
-        if let Some(z) = &n.zenoh {
-            z.modules.iter().for_each(|m| {
-                if !known_dependencies.contains(m) {
-                    panic!("KnownNode {} is using an unknown module {}", n.name, m);
+    for (name, node) in nodes {
+        if let Some(zenoh) = &node.zenoh {
+            for module in &zenoh.modules {
+                if !known_dependencies.contains(module) {
+                    return Err(
+                        format!("Node {} is using an unknown module {}", name, module).into(),
+                    );
                 }
-            });
+            }
         }
-    });
-
-    println!("KnownNode Library is valid and ready to be used");
-
-    // ----
-    //
-    let btree = deserialize_behaviour_tree("btree.yaml").expect("Failed to deserialize tree");
-    //println!("{:#?}", btree);
-
-    let mut names = Vec::new();
-    for node in &btree.tree {
-        extract_names(node, &mut names);
     }
-    println!("Nodes in Tree File: {:?}", names);
-    println!("btree.yaml is valid and parsed correctly");
+    println!("KnownNode Library is valid and ready to be used");
+    Ok(())
+}
 
-    // ----
-    //
-    btree.tree.iter().for_each(|n| {
-        if !names.contains(&n.name) {
-            panic!("Node {} is not defined in the KnownNode Library", n.name);
+fn load_library(library_path: &PathBuf) 
+    -> Result<Library, Box<dyn std::error::Error>> 
+{
+    let modules = load_file_modules(library_path.join("modules.yaml").to_str().unwrap()).expect("Failed to load modules");
+    let tools = load_file_tools(library_path.join("tools.yaml").to_str().unwrap()).expect("Failed to load tools");
+    let known_dependencies = dependencies_abbr(&modules, &tools);
+
+    let nodes = load_file_nodes(library_path.join("nodes.yaml").to_str().unwrap()).expect("Failed to load nodes");
+    validate_nodes_library(&nodes, &known_dependencies).expect("Failed to validate nodes library");
+    Ok(Library {
+        modules,
+        tools,
+        nodes,
+    })
+}
+
+fn validate_btree(
+    tree_file: &BehaviorTreeFile,
+    library: &Library,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // Check if btree name is not a known node
+    if library.nodes.contains_key(&tree_file.tree.name) {
+        return Err(format!(
+            "Behavior Tree name {} is already used by a known node",
+            tree_file.tree.name
+        )
+        .into());
+    }
+
+    // Check if participants are known
+    for participant in &tree_file.participants {
+        if !library.modules.contains_key(participant) {
+            return Err(format!("Participant Module {} is not a known node", participant).into());
         }
-    });
+    }
+
+    validate_node(&tree_file.tree, &library).expect("Failed to validate node");
+
+    println!("Behavior Tree file {} is valid and ready to be used", tree_file.tree.name);
 
     Ok(())
 }
 
+fn validate_node(tree: &Node, library: &Library) -> Result<(), Box<dyn std::error::Error>> {
+    // Check if the tree is valid
+    // 1. If the node has children, check if they are valid
+    //  a. If a node has no children, check if it is a known node
+    //  b. If all children are valid, the node is valid
+    // 2. If node has fallback, check if all fallback nodes are valid
+    // 3. If all nodes are valid, the tree is valid
+
+    if let Some(fallback) = &tree.fallback {
+        // If the node has fallback, check if all fallback nodes are valid
+        for child in fallback {
+            //  If all nodes are valid, the tree is valid
+            validate_node(child, library)?;
+        }
+    } else if let Some(children) = &tree.children {
+        // If the node has children, check if they are valid
+        for child in children {
+            //  If all children are valid, the node is valid
+            validate_node(child, library)?;
+        }
+    } else {
+        // If a node has no children, check if it is a known node
+        if !library.nodes.contains_key(&tree.name) {
+            return Err(format!("Node {} is not a known node", tree.name).into());
+        }
+    }
+
+    Ok(())
+}
+
+fn library_path() -> Result<PathBuf, Box<dyn std::error::Error>> {
+    let mut home_dir = dirs::home_dir().expect("Unable to find home directory");
+    home_dir.push("genotyper");
+    home_dir.push("library");
+    Ok(home_dir)
+}
+
+fn main() -> Result<(), serde_yaml::Error> {
+    // Load the library
+    let library_path = library_path().expect("Unable to find library path");
+    let library = load_library(&library_path).expect("Failed to load library");
+
+    // Load the btree
+    let mut btree_file_path = library_path.clone();
+    btree_file_path.push("trees");
+    btree_file_path.push("attempt_pickup_tip-0.0.1.yaml");
+    let btree_file = load_a_behaviour_tree(btree_file_path.to_str().unwrap()).expect("Failed to deserialize tree");
+    validate_btree(&btree_file, &library).expect("Failed to validate tree");
+
+    //let mut names = Vec::new();
+    //extract_names(&btree_file.tree, &mut names);
+    //println!("Nodes in Tree File: {:?}", names);
+    println!("btree.yaml is valid and parsed correctly");
+
+    Ok(())
+}
