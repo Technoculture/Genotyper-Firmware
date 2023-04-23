7f248505c24bfa45013c879bfab4a03327811283
Satyam Tiwary <satyam@technoculture.io>
2023-04-23 22:17:48 +0530

    chore: starting with executors

--- workflow/bin/executor.rs
+++ workflow/bin/executor.rs
@@ -1,9 +1,10 @@
 use clap::Parser;
 use workflow::conf::{
-    get_tree_by_name, load_library, root_library_path, BehaviorTreeFile, Library, Node
+    get_tree_by_name, load_library, root_library_path, BehaviorTreeFile, Library, Node,
 };
+use workflow::Sequence;
 
-use log::{debug, trace, info};
+use log::{debug, info, trace};
 use simplelog::*;
 use std::error::Error;
 
@@ -16,32 +17,45 @@ struct Args {
     workflow_name: String,
 
     /// The name of the tree to run
-    #[arg(short, long, default_value = "attempt_pickup_tip")]
+    #[arg(short, long, default_value = "get_tip")]
     // tree_name: Option<String>,
     tree_name: String,
 }
 
-//pub fn execute_node(node: &Node, library: &Library) -> Result<(), Box<dyn Error>> {
-//    // Execute the current node
-//    info!("Executing node: '{}'", node.name);
-//
-//    // TODO: Implement the actual node execution logic here
-//
-//    // Recursively execute child nodes
-//    for child in &node.children {
-//        execute_node(child, library)?;
-//    }
-//
-//    Ok(())
-//}
+pub fn execute_node(node: &Node, library: &Library) -> Result<(), Box<dyn Error>> {
+    // Execute the current node
+    trace!("Executing node: '{}'", node.name);
+
+    // TODO: Implement the actual node execution logic here
+    // Recursively execute child nodes
+    if let Some(nodes) = &node.sequence {
+        match nodes {
+            Sequence::Fallback(fallback_nodes) => {
+                for node in fallback_nodes {
+                    execute_node(node, library)?;
+                }
+            }
+            Sequence::Children(children_nodes) => {
+                for node in children_nodes {
+                    execute_node(node, library)?;
+                }
+            }
+        }
+    } else {
+        debug!("Executing leaf node: {}", node.name);
+    }
+    Ok(())
+}
 
 pub fn execute_tree(tree: &BehaviorTreeFile, library: &Library) -> Result<(), Box<dyn Error>> {
-    // println!("{:#?}", &tree);
-    info!("Starting execution of a tree: '{}' ({})", tree.title, tree.description);
+    info!(
+        "Starting execution of a tree: '{}' ({})",
+        tree.title, tree.description
+    );
 
     // TODO: Traverse through the tree and execute the nodes
     // Traverse in a root-first manner
-
+    execute_node(&tree.tree, library)?;
 
     Ok(())
 }
