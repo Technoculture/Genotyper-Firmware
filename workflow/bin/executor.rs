e6664ede679e94b7a987a4f1c614d50dc0f8df4c
Satyam Tiwary <satyam@technoculture.io>
2023-04-26 23:32:44 +0530

    chore: minimal terrible executor

--- workflow/bin/executor.rs
+++ workflow/bin/executor.rs
@@ -1,18 +1,26 @@
 use clap::Parser;
 use workflow::conf::{
-    get_tree_by_name, load_library, root_library_path, BehaviorTreeFile, Library, Node,
+    get_tree_by_name, load_library, root_library_path, BehaviorTreeFile, Library, ModuleFile, Node,
 };
 use workflow::Sequence;
-
 use log::{debug, info, trace};
 use simplelog::*;
 use std::error::Error;
+
+use async_std::prelude::*;
+use async_std::channel::{bounded, Receiver, Sender};
+use async_std::task;
+use std::io::{stdin, BufRead};
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
+use std::thread;
+use async_recursion::async_recursion;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// The name of the workflow to run
-    #[arg(short, long, default_value = "TB PCR")]
+    #[arg(short, long, default_value = "tb_pcr")]
     // workflow_name: Option<String>,
     workflow_name: String,
 
@@ -22,45 +30,54 @@ struct Args {
     tree_name: String,
 }
 
-pub fn execute_node(node: &Node, library: &Library) -> Result<(), Box<dyn Error>> {
-    // Execute the current node
-    trace!("Executing node: '{}'", node.name);
+async fn module_task(receiver: Receiver<Node>, modules: &ModuleFile) {
+    while let Ok(node) = receiver.recv().await {
+        execute_leaf_node(&node, modules).await;
+    }
+}
 
+async fn execute_leaf_node(node: &Node, modules: &ModuleFile) -> Result<(), Box<dyn Error>> {
+    info!("Executing leaf node: {}", node.name);
+    async_std::task::sleep(std::time::Duration::from_millis(200)).await;
+    Ok(())
+}
+
+#[async_recursion]
+pub async fn execute_node(node: &Node, library: &Library) -> Result<(), Box<dyn Error>> {
+    trace!("Executing node: '{}'", node.name);
     // TODO: Implement the actual node execution logic here
     // Recursively execute child nodes
     if let Some(nodes) = &node.sequence {
         match nodes {
             Sequence::Fallback(fallback_nodes) => {
                 for node in fallback_nodes {
-                    execute_node(node, library)?;
+                    execute_node(node, library).await?;
                 }
             }
             Sequence::Children(children_nodes) => {
                 for node in children_nodes {
-                    execute_node(node, library)?;
+                    execute_node(node, library).await?;
                 }
             }
         }
     } else {
         trace!("Executing leaf node: {}", node.name);
+        // async_std::task::block_on(execute_leaf_node(node, &library.modules))?;
+        execute_leaf_node(node, &library.modules).await?;
     }
     Ok(())
 }
 
-pub fn execute_tree(tree: &BehaviorTreeFile, library: &Library) -> Result<(), Box<dyn Error>> {
+async fn execute_tree(tree: &BehaviorTreeFile, library: &Library) -> Result<(), Box<dyn Error>> {
     info!(
         "Starting execution of a tree: '{}' ({})",
         tree.title, tree.description
     );
-
-    // TODO: Traverse through the tree and execute the nodes
-    // Traverse in a root-first manner
-    execute_node(&tree.tree, library)?;
-
-    Ok(())
+    execute_node(&tree.tree, library).await
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Initialise the logger
     CombinedLogger::init(vec![TermLogger::new(
         LevelFilter::Info,
@@ -81,7 +98,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // trace!("{:?}", out);
 
     let tree = get_tree_by_name(&args.tree_name, &library).expect("Failed to get tree");
-    execute_tree(&tree, &library)?;
-
-    Ok(())
+    execute_tree(&tree, &library).await
 }
